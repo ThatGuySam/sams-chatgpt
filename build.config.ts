bfd2444e1b7d097677708bf1ce344c74ab4699a4
Sam Carlton <sam@sam.lc>
2023-03-13 19:39:59 -0500

    Lint existing code

--- build.config.ts
+++ build.config.ts
@@ -1,12 +1,12 @@
 import { defineBuildConfig } from 'unbuild'
 
-export default defineBuildConfig({
-  entries: [
-    'src/index',
-  ],
-  declaration: true,
-  clean: true,
-  rollup: {
-    emitCJS: true,
-  },
-})
+export default defineBuildConfig( {
+    entries: [
+        'src/index',
+    ],
+    declaration: true,
+    clean: true,
+    rollup: {
+        emitCJS: true,
+    },
+} )
